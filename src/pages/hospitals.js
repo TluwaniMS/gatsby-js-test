617fcfa1bbbfd6203306383b774de1e4b41ebd67
tluwaniMS <tluwani2020@gmail.com>
2022-01-12 11:57:05 +0200

    adding-page-views

---
+++ src/pages/hospitals.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const hospitals = () => {
+  return <div></div>;
+};
+
+export default hospitals;
---
+++ src/pages/municipalities.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const municipalities = () => {
+  return <div></div>;
+};
+
+export default municipalities;
