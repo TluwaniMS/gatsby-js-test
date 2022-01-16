fd21a8962f7012336471f7e355a27bbc14f7b4ac
tluwaniMS <tluwani2020@gmail.com>
2022-01-16 09:56:32 +0200

    adding-municipality-image-to-display

--- src/components/municipalities-component/MunicipalitiesView.js
+++ src/components/municipalities-component/MunicipalitiesView.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./MunicipalitiesView.css";
+import municipalityImage from "../../images/city-hall.png";
 
 const MunicipalitiesView = ({ municipalities }) => {
   return (
     <div className="main-content-container">
       {municipalities.map((municipality) => (
         <div className="card" key={municipality.municipalityKey}>
-          <h4>{municipality.municipalityName}</h4>
+          <img src={municipalityImage} className="municipality-image" alt="Avatar"></img>
+          <div className="municipality-container">
+            <h4>{municipality.municipalityName}</h4>
+          </div>
         </div>
       ))}
     </div>
