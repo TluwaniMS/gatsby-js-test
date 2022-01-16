8d048b6de742d47510c61f6894b8aefde6d9f3a4
tluwaniMS <tluwani2020@gmail.com>
2022-01-16 15:43:57 +0200

    adding-display-updates-to-home-page

---
+++ src/components/home-page-component/HomePageView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePageView.css";
+
+const HomePageView = () => {
+  return (
+    <div className="welcome-page-main-content-container">
+      <h1>Welcome to the doctors directory.</h1>
+    </div>
+  );
+};
+
+export default HomePageView;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import Layout from "../components/layout-component/Layout";
+import HomePageView from "../components/home-page-component/HomePageView";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
-      <h1>Home Page Works!!</h1>
+      <HomePageView />
     </Layout>
   );
 };
