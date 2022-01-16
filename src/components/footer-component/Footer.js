5209a3c18d4b535a0522bf64b7e313d66c5a579b
tluwaniMS <tluwani2020@gmail.com>
2022-01-16 14:35:42 +0200

    adding-footer

---
+++ src/components/footer-component/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return <div className="main-footer-container">&#169; TluwaniMS</div>;
+};
+
+export default Footer;
--- src/components/layout-component/Layout.js
+++ src/components/layout-component/Layout.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Navbar from "../navbar-component/Navbar";
+import Footer from "../footer-component/Footer";
 import "./Layout.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div className="">
+    <div className="main-layout-container">
       <title>{pageTitle}</title>
       <Navbar />
       <main>
         <h1 className="page-title-container">{pageTitle}</h1>
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
