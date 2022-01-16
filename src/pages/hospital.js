87cc1d43de35b397a58d7cf57f269df925b7495c
tluwaniMS <tluwani2020@gmail.com>
2022-01-16 15:55:52 +0200

    adding-detailed-view-pages

---
+++ src/pages/hospital.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hospital = () => {
+  return <div></div>;
+};
+
+export default Hospital;
---
+++ src/pages/municipality.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Municipality = () => {
+  return <div></div>;
+};
+
+export default Municipality;
