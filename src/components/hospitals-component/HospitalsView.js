f3417973e15ef1635a3a88b0bfb00f9895d63938
tluwaniMS <tluwani2020@gmail.com>
2022-01-16 09:31:47 +0200

    adding-hospital-image-to-display

--- src/components/hospitals-component/HospitalsView.js
+++ src/components/hospitals-component/HospitalsView.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./HospitalsView.css";
+import hospitalImage from "../../images/kindpng_2646334.png";
 
 const HospitalsView = ({ hospitals }) => {
   return (
     <div className="main-content-container">
       {hospitals.map((hospital) => (
         <div className="hospitals-card" key={hospital.hospitalKey}>
-          <h4>{hospital.hospitalName}</h4>
+          <img src={hospitalImage} className="hospital-image" alt="Avatar"></img>
+          <div className="hospitals-container">
+            <h4>{hospital.hospitalName}</h4>
+          </div>
         </div>
       ))}
     </div>
