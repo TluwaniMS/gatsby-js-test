966ad9fc862881ec2768b9b2c39484d3661e2a30
tluwaniMS <tluwani2020@gmail.com>
2022-01-17 17:04:41 +0200

    adding-detailed-hospitals-data-view

--- src/components/hospital-component/HospitalView.js
+++ src/components/hospital-component/HospitalView.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./HospitalView.css";
+import hospitalImage from "../../images/kindpng_2646334.png";
 
 const HospitalView = ({ detailedHospitalInformation }) => {
   console.log(detailedHospitalInformation);
 
   return (
-    <div>
-      <h4>Hosptal view works!!</h4>
+    <div className="main-detailed-hospital-information-container">
+      <div className="detailed-hospital-card">
+        <img src={hospitalImage} className="hospitals-image" alt="Avatar"></img>
+        <div className="detailed-information-container">
+          <div className="information-label">{detailedHospitalInformation.hospitalName}</div>
+        </div>
+      </div>
     </div>
   );
 };
