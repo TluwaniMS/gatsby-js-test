658e15af9fc2cd5d4eca9458ce8154f44a6abeda
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 10:31:56 +0200

    adding-spinner-view-componet

---
+++ src/components/spinner-component/SpinnerView.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SpinnerView.css";
+
+const SpinnerView = () => {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SpinnerView;
