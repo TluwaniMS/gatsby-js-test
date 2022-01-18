287feef9cddd08e89b92461c92fbf590f9ae2cb1
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 11:11:14 +0200

    adding-detailed-municipality-view-and-styling

--- src/components/municipality-component/MunicipalityView.js
+++ src/components/municipality-component/MunicipalityView.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./MunicipalityView.css";
+import municipalityImage from "../../images/city-hall.png";
 
 const MunicipalityView = ({ detailedMunicipalityView }) => {
   console.log(detailedMunicipalityView);
 
   return (
-    <div>
-      <h4>Municipality view works!!</h4>
+    <div className="main-detailed-municipality-information-container">
+      <div className="card">
+        <img src={municipalityImage} className="municipality-image" alt="Avatar"></img>
+        <div className="municipality-container">
+          <h4>{detailedMunicipalityView.municipalityName}</h4>
+        </div>
+      </div>
     </div>
   );
 };
