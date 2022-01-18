b8baa5c026f3dbac098ceb5587d375349af4ff6e
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 21:53:35 +0200

    adding-font-awesome-iicons

--- src/components/navbar-component/Navbar.js
+++ src/components/navbar-component/Navbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "gatsby";
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserMd, faHospitalAlt, faHospital } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <main>
       <div className="main-navbar-container">
         <Link to="/doctors" className="navbar-item">
+          <FontAwesomeIcon icon={faUserMd} />
           doctors
         </Link>
         <Link to="/hospitals" className="navbar-item">
+          <FontAwesomeIcon icon={faHospital} />
           hospitals
         </Link>
         <Link to="/municipalities" className="navbar-item">
+          <FontAwesomeIcon icon={faHospitalAlt} />
           municipalities
         </Link>
       </div>
