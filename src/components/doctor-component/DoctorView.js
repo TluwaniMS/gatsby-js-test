84994aae59042232f6785c4cf6ab4c180317af25
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 14:05:42 +0200

    adding-detailed-doctors-view-display

--- src/components/doctor-component/DoctorView.js
+++ src/components/doctor-component/DoctorView.js
@@ -1,28 +1,13 @@
 import React from "react";
 import "./DoctorView.css";
-import femaleDoctorImage from "../../images/female-doctor.jpg";
-import maleDoctorImage from "../../images/2716211571598811054-128.png";
+import DetailedDoctorsDisplay from "../../displays/detailed-doctors-display/DetailedDoctorsDisplay";
 
 const DoctorView = ({ detailedDoctorsInformation }) => {
   console.log(detailedDoctorsInformation);
 
   return (
     <div className="main-detailed-doctor-information-container">
-      <div className="detailed-doctor-card">
-        <img
-          src={detailedDoctorsInformation.gender === "Female" ? femaleDoctorImage : maleDoctorImage}
-          className="doctors-image"
-          alt="Avatar"
-        ></img>
-        <div className="detailed-information-container">
-          <div className="information-label">Name:</div>
-          <div className="information">{`${detailedDoctorsInformation.firstName} ${detailedDoctorsInformation.lastName}`}</div>
-          <div className="information-label">Email:</div>
-          <div className="information">{detailedDoctorsInformation.email}</div>
-          <div className="information-label">gender:</div>
-          <div className="information">{detailedDoctorsInformation.gender}</div>
-        </div>
-      </div>
+      <DetailedDoctorsDisplay detailedDoctorsInformation={detailedDoctorsInformation} />
     </div>
   );
 };
--- src/displays/detailed-doctors-display/DetailedDoctorsDisplay.js
+++ src/displays/detailed-doctors-display/DetailedDoctorsDisplay.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./DetailedDoctorsDisplay.css";
+import femaleDoctorImage from "../../images/female-doctor.jpg";
+import maleDoctorImage from "../../images/2716211571598811054-128.png";
 
-const DetailedDoctorsDisplay = () => {
-  return <div></div>;
+const DetailedDoctorsDisplay = ({ detailedDoctorsInformation }) => {
+  return (
+    <div className="detailed-doctor-card">
+      <img
+        src={detailedDoctorsInformation.gender === "Female" ? femaleDoctorImage : maleDoctorImage}
+        className="doctors-image"
+        alt="Avatar"
+      ></img>
+      <div className="detailed-information-container">
+        <div className="information-label">Name:</div>
+        <div className="information">{`${detailedDoctorsInformation.firstName} ${detailedDoctorsInformation.lastName}`}</div>
+        <div className="information-label">Email:</div>
+        <div className="information">{detailedDoctorsInformation.email}</div>
+        <div className="information-label">gender:</div>
+        <div className="information">{detailedDoctorsInformation.gender}</div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailedDoctorsDisplay;
