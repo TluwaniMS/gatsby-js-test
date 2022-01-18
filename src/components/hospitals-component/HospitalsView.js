94c94c2a27f20e3ab3a1397e4025bffb25a0d9bb
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 12:25:15 +0200

    adding-hospital-content-display-view

--- src/components/hospitals-component/HospitalsView.js
+++ src/components/hospitals-component/HospitalsView.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./HospitalsView.css";
-import hospitalImage from "../../images/kindpng_2646334.png";
-import { Link } from "gatsby";
+import HospitalDisplay from "../../displays/hospital-display/HospitalDisplay";
 
 const HospitalsView = ({ hospitals }) => {
   return (
     <div className="main-content-container">
       {hospitals.map((hospital) => (
-        <Link to={`/hospital/${hospital.hospitalKey}`} className="hospitals-card" key={hospital.hospitalKey}>
-          <img src={hospitalImage} className="hospital-image" alt="Avatar"></img>
-          <div className="hospitals-container">
-            <h4>{hospital.hospitalName}</h4>
-          </div>
-        </Link>
+        <div key={hospital.hospitalKey} className="hospitals-card">
+          <HospitalDisplay hospital={hospital} />
+        </div>
       ))}
     </div>
   );
--- src/displays/hospital-display/HospitalDisplay.js
+++ src/displays/hospital-display/HospitalDisplay.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./HospitalDisplay.css";
+import { Link } from "gatsby";
+import hospitalImage from "../../images/kindpng_2646334.png";
 
-const HospitalDisplay = () => {
-  return <div></div>;
+const HospitalDisplay = ({ hospital }) => {
+  return (
+    <Link to={`/hospital/${hospital.hospitalKey}`}>
+      <img src={hospitalImage} className="hospital-image" alt="Avatar"></img>
+      <div className="hospitals-container">
+        <h4>{hospital.hospitalName}</h4>
+      </div>
+    </Link>
+  );
 };
 
 export default HospitalDisplay;
