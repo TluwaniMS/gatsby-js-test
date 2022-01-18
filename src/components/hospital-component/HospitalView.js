32393bc56cb4f455652dac03051ee87c4f4bdb7c
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 15:02:33 +0200

    adding-detailed-hospital-display-view

--- src/components/hospital-component/HospitalView.js
+++ src/components/hospital-component/HospitalView.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./HospitalView.css";
-import hospitalImage from "../../images/kindpng_2646334.png";
+import DetailedHospitalDisplay from "../../displays/detailed-hospital-hospital/DetailedHospitalDisplay";
 
 const HospitalView = ({ detailedHospitalInformation }) => {
   console.log(detailedHospitalInformation);
 
   return (
     <div className="main-detailed-hospital-information-container">
-      <div className="detailed-hospital-card">
-        <img src={hospitalImage} className="hospitals-image" alt="Avatar"></img>
-        <div className="detailed-information-container">
-          <div className="information-label">{detailedHospitalInformation.hospitalName}</div>
-        </div>
-      </div>
+      <DetailedHospitalDisplay detailedHospitalInformation={detailedHospitalInformation} />
     </div>
   );
 };
--- src/displays/detailed-hospital-hospital/DetailedHospitalDisplay.js
+++ src/displays/detailed-hospital-hospital/DetailedHospitalDisplay.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./DetailedHospitalDisplay.css";
+import hospitalImage from "../../images/kindpng_2646334.png";
 
-const DetailedHospitalDisplay = () => {
-  return <div></div>;
+const DetailedHospitalDisplay = ({ detailedHospitalInformation }) => {
+  return (
+    <div className="detailed-hospital-card">
+      <img src={hospitalImage} className="hospitals-image" alt="Avatar"></img>
+      <div className="detailed-information-container">
+        <div className="information-label">{detailedHospitalInformation.hospitalName}</div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailedHospitalDisplay;
