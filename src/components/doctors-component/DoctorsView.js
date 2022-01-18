8e2924a74341378a221015476e03c6542cdc973a
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 13:06:55 +0200

    adding-doctors-display-view

--- src/components/doctors-component/DoctorsView.js
+++ src/components/doctors-component/DoctorsView.js
@@ -1,24 +1,14 @@
 import React from "react";
 import "./DoctorsView.css";
-import femaleDoctorImage from "../../images/female-doctor.jpg";
-import maleDoctorImage from "../../images/2716211571598811054-128.png";
-import { Link } from "gatsby";
+import DoctorsDisplay from "../../displays/doctors-display/DoctorsDisplay";
 
 const DoctorsView = ({ doctors }) => {
   return (
     <div className="main-content-container">
       {doctors.map((doctor) => (
-        <Link to={`/doctor/${doctor.id}`} className="doctors-card " key={doctor.id}>
-          <img
-            src={doctor.gender === "Female" ? femaleDoctorImage : maleDoctorImage}
-            className="doctors-image"
-            alt="Avatar"
-          ></img>
-          <div className="names-container">
-            <span className="name-container">{doctor.firstName}</span>
-            <span className="name-container">{doctor.lastName}</span>
-          </div>
-        </Link>
+        <div className="doctors-card" key={doctor.id}>
+          <DoctorsDisplay doctor={doctor} />
+        </div>
       ))}
     </div>
   );
--- src/displays/doctors-display/DoctorsDisplay.js
+++ src/displays/doctors-display/DoctorsDisplay.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Link } from "gatsby";
 import "./DoctorsDisplay.css";
+import femaleDoctorImage from "../../images/female-doctor.jpg";
+import maleDoctorImage from "../../images/2716211571598811054-128.png";
 
-const DoctorsDisplay = () => {
-  return <div></div>;
+const DoctorsDisplay = ({ doctor }) => {
+  return (
+    <Link to={`/doctor/${doctor.id}`}>
+      <img
+        src={doctor.gender === "Female" ? femaleDoctorImage : maleDoctorImage}
+        className="doctors-image"
+        alt="Avatar"
+      ></img>
+      <div className="names-container">
+        <span className="name-container">{doctor.firstName}</span>
+        <span className="name-container">{doctor.lastName}</span>
+      </div>
+    </Link>
+  );
 };
 
 export default DoctorsDisplay;
