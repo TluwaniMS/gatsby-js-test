27e9782accf2abf9b1d09eee0700f43ad25e99e9
tluwaniMS <tluwani2020@gmail.com>
2022-01-18 14:45:31 +0200

    adding-detailed-municipality-display-view

--- src/components/municipality-component/MunicipalityView.js
+++ src/components/municipality-component/MunicipalityView.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./MunicipalityView.css";
-import municipalityImage from "../../images/city-hall.png";
+import DetailedMunicipalityDisplay from "../../displays/detailed-municipality-display/DetailedMunicipalityDisplay";
 
 const MunicipalityView = ({ detailedMunicipalityView }) => {
   console.log(detailedMunicipalityView);
 
   return (
     <div className="main-detailed-municipality-information-container">
-      <div className="card">
-        <img src={municipalityImage} className="municipality-image" alt="Avatar"></img>
-        <div className="municipality-container">
-          <h4>{detailedMunicipalityView.municipalityName}</h4>
-        </div>
-      </div>
+      <DetailedMunicipalityDisplay detailedMunicipalityView={detailedMunicipalityView} />
     </div>
   );
 };
--- src/displays/detailed-municipality-display/DetailedMunicipalityDisplay.js
+++ src/displays/detailed-municipality-display/DetailedMunicipalityDisplay.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./DetailedMunicipalityDisplay.css";
+import municipalityImage from "../../images/city-hall.png";
 
-const DetailedMunicipalityDisplay = () => {
-  return <div></div>;
+const DetailedMunicipalityDisplay = ({ detailedMunicipalityView }) => {
+  return (
+    <div className="card">
+      <img src={municipalityImage} className="municipality-image" alt="Avatar"></img>
+      <div className="municipality-container">
+        <h4>{detailedMunicipalityView.municipalityName}</h4>
+      </div>
+    </div>
+  );
 };
 
 export default DetailedMunicipalityDisplay;
