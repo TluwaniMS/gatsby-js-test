8dc54df501fb03b60003e67adc20e52bd5fcd914
tluwaniMS <tluwani2020@gmail.com>
2022-01-20 11:46:12 +0200

    adding-sample-support-data

---
+++ src/display-support/hospital-data.js
@@ -0,0 +1,54 @@
+export const Hospitals = [
+  {
+    hospitalName: "Sekhukhune Private Hospital",
+    hospitalKey: "SKKN"
+  },
+  {
+    hospitalName: "Shapo Private Hospital",
+    hospitalKey: "SHPO"
+  },
+  {
+    hospitalName: "Polokwane Hospital",
+    hospitalKey: "PLKH"
+  },
+  {
+    hospitalName: "Botlokwa Hospital",
+    hospitalKey: "BTLK"
+  },
+  {
+    hospitalName: "Marobjyane Private Hospital",
+    hospitalKey: "MRBJ"
+  },
+  {
+    hospitalName: "Ntsundeni Private Hospital",
+    hospitalKey: "NTNP"
+  },
+  {
+    hospitalName: "Moshe Hospital",
+    hospitalKey: "MSHH"
+  },
+  {
+    hospitalName: "Marabastad Hospital",
+    hospitalKey: "MRSB"
+  },
+  {
+    hospitalName: "Baloyi Hospital",
+    hospitalKey: "BLYH"
+  },
+  {
+    hospitalName: "Vhatwanamba Private Health Care",
+    hospitalKey: "VTPH"
+  },
+  {
+    hospitalName: "Bulombo Hospital",
+    hospitalKey: "BLMH"
+  },
+  {
+    hospitalName: "Mashashane Hospital",
+    hospitalKey: "MSSH"
+  },
+  {
+    hospitalName: "dendron Health Centre",
+    hospitalKey: "DNDH"
+  }
+];
