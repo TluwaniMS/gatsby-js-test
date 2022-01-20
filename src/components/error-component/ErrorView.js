7b131890fed2355f370c6f654413abb9a95502f3
tluwaniMS <tluwani2020@gmail.com>
2022-01-20 15:01:58 +0200

    adding-error-display-styling

--- src/components/error-component/ErrorView.js
+++ src/components/error-component/ErrorView.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./ErrorView.css";
 
 const ErrorView = () => {
-  return <div>An error was encountered in the process of retrieving the requested data, please try again later.</div>;
+  return (
+    <div className="error-display-container">
+      An error was encountered in the process of retrieving the requested data, please try again later.
+    </div>
+  );
 };
 
 export default ErrorView;
