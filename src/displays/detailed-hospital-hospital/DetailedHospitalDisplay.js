6fb936f41ef220eb7925c617c18fa3babc416acb
tluwaniMS <tluwani2020@gmail.com>
2022-01-20 12:48:06 +0200

    adding-municipality-name-to-detailed-hospitals-view

--- src/displays/detailed-hospital-hospital/DetailedHospitalDisplay.js
+++ src/displays/detailed-hospital-hospital/DetailedHospitalDisplay.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./DetailedHospitalDisplay.css";
 import hospitalImage from "../../images/kindpng_2646334.png";
+import { Municipalities } from "../../display-support/municipality-data";
 
 const DetailedHospitalDisplay = ({ detailedHospitalInformation }) => {
+  const municipalityName = Municipalities.filter(
+    (municipality) => municipality.municipalityKey === detailedHospitalInformation.municipality
+  ).map((municipality) => municipality.municipalityName)[0];
+
   return (
     <div className="detailed-hospital-card">
       <img src={hospitalImage} className="hospitals-image" alt="Avatar"></img>
       <div className="detailed-information-container">
         <div className="information-label">{detailedHospitalInformation.hospitalName}</div>
+        <div className="information-label">municipality:</div>
+        <div className="information">{municipalityName}</div>
       </div>
     </div>
   );
