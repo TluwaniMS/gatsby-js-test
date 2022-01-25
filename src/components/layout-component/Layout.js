05a3cd8dc68b29da37b1b0bcf03bb92a5b591913
tluwaniMS <tluwani2020@gmail.com>
2022-01-25 11:44:42 +0200

    adding-view-icons

--- src/components/layout-component/Layout.js
+++ src/components/layout-component/Layout.js
@@ -1,16 +1,38 @@
 import React from "react";
 import "./Layout.css";
+import { DataType } from "../../display-support/data-type";
 import { ApolloProvider } from "@apollo/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserMd, faHospitalAlt, faHospital } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../navbar-component/Navbar";
 import Footer from "../footer-component/Footer";
 
 const Layout = ({ pageTitle, children }) => {
+  const DoctorsView = pageTitle === DataType.Doctors || pageTitle === DataType.Doctor;
+  const HospitalsView = pageTitle === DataType.Hospitals || pageTitle === DataType.Hospital;
+  const MunicipalitiesView = pageTitle === DataType.Municipalities || pageTitle === DataType.Municipality;
+
   return (
     <ApolloProvider>
       <div className="main-layout-container">
         <title>{pageTitle}</title>
         <Navbar />
         <main>
+          {DoctorsView && (
+            <div className="view-icon">
+              <FontAwesomeIcon icon={faUserMd} />
+            </div>
+          )}
+          {HospitalsView && (
+            <div className="view-icon">
+              <FontAwesomeIcon icon={faHospital} />
+            </div>
+          )}
+          {MunicipalitiesView && (
+            <div className="view-icon">
+              <FontAwesomeIcon icon={faHospitalAlt} />
+            </div>
+          )}
           <h1 className="page-title-container">{pageTitle}</h1>
           {children}
         </main>
