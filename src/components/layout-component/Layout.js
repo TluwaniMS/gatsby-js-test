d2770350d121a09e3971f86ada2f21d84553a3fc
tluwaniMS <tluwani2020@gmail.com>
2022-01-25 02:54:03 +0200

    adding-apollo-provider

--- src/components/layout-component/Layout.js
+++ src/components/layout-component/Layout.js
@@ -2,18 +2,21 @@ import React from "react";
 import Navbar from "../navbar-component/Navbar";
 import Footer from "../footer-component/Footer";
 import "./Layout.css";
+import { ApolloProvider } from "@apollo/client";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div className="main-layout-container">
-      <title>{pageTitle}</title>
-      <Navbar />
-      <main>
-        <h1 className="page-title-container">{pageTitle}</h1>
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <ApolloProvider>
+      <div className="main-layout-container">
+        <title>{pageTitle}</title>
+        <Navbar />
+        <main>
+          <h1 className="page-title-container">{pageTitle}</h1>
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 };
 
