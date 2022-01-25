f8b6bf78c3fb2d584dd84558271252a9afd7a326
tluwaniMS <tluwani2020@gmail.com>
2022-01-25 03:04:05 +0200

    adding-hospital-mutation

--- src/displays/detailed-hospital-hospital/DetailedHospitalDisplay.js
+++ src/displays/detailed-hospital-hospital/DetailedHospitalDisplay.js
@@ -5,13 +5,21 @@ import { Municipalities } from "../../display-support/municipality-data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { navigate } from "@reach/router";
+import { GET_ALL_HOSPITALS } from "../../queries/hospitals.query";
+import { DELETE_HOSPITAL_BY_ID } from "../../mutations/hospitals.mutation";
+import { useMutation } from "@apollo/client";
 
 const DetailedHospitalDisplay = ({ detailedHospitalInformation }) => {
+  const [deleteHospitalById, { data, loading, error }] = useMutation(DELETE_HOSPITAL_BY_ID, {
+    refetchQueries: [GET_ALL_HOSPITALS]
+  });
+
   const municipalityName = Municipalities.filter(
     (municipality) => municipality.municipalityKey === detailedHospitalInformation.municipality
   ).map((municipality) => municipality.municipalityName)[0];
 
   const deleteHospital = (hospitalId) => {
+    deleteHospitalById({ variables: { hospitalKey: hospitalId } });
     navigate("/hospitals", { replace: true });
   };
 
